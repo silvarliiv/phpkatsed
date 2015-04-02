31a86da2818a0710b10036ec655fc581f56f0c7b
Silvar Liiv <silvar.liiv@tigu.hk.tlu.ee>
2015-04-02 16:06:56 +0300

    mtsds

--- menyy.php
+++ menyy.php
@@ -14,6 +14,7 @@
 	 <?php 
 	   $riigid=array("Soome", "Eesti", "Läti", "Leedu");
 	   $valikunr=2;
+	   if(isSet($_REQUEST["riigivalik"])){$valikunr=intval($_REQUEST["riigivalik"]);}
 	   for($riiginr=0; $riiginr<count($riigid); $riiginr++){
 	      $lisand=($riiginr==$valikunr) ? "selected='selected'" : "";
 	      echo "<option value='$riiginr' $lisand>$riigid[$riiginr]</option>\n";
