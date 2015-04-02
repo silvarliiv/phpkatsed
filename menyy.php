6801b1cf1efafc8a8b1dcebc5bb76183f2df6c80
Silvar Liiv <silvar.liiv@tigu.hk.tlu.ee>
2015-04-02 15:45:59 +0300

    mtsds

---
+++ menyy.php
@@ -0,0 +1,26 @@
+﻿<!doctype html>
+<html>
+  <head>
+    <title>Menüü proov</title>
+  </head>
+  <body>
+     <select name="maakonnad">
+	   <option>Hiiumaa</option>
+	   <option selected="selected">Läänemaa</option>
+	   <option>Harjumaa</option>
+	 </select>
+	 <form action="?">
+	  <select name="riigivalik">
+	 <?php 
+	   $riigid=array("Soome", "Eesti", "Läti", "Leedu");
+	   $valikunr=2;
+	   for($riiginr=0; $riiginr<count($riigid); $riiginr++){
+	      $lisand=($riiginr==$valikunr) ? "selected='selected'" : "";
+	      echo "<option value='$riiginr' $lisand>$riigid[$riiginr]</option>\n";
+	   }
+	 ?>
+	  </select>
+	  <input type="submit" value="sisesta" />
+	 </form>
+  </body>
+</html>
